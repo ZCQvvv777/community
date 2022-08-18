b765886a9213885b10a149a8113e524de1b511b4
ZCQvvv777 <swjtu2016@126.com>
2022-08-18 11:30:36 +0800

    增加邮件发送功能

---
+++ community/src/test/java/com/ZCQvvv777/community/MailTests.java
@@ -0,0 +1,42 @@
+package com.ZCQvvv777.community;
+
+import com.ZCQvvv777.community.util.MailClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+/**
+ * @Description TODO
+ * @Date 2022/8/18 10:51
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MailTests {
+    @Autowired
+    private MailClient mailClient;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Test
+    public void testTextMail() {
+        mailClient.sendMail("ZCQvvv777@buaa.edu.cn", "TEST","Hello!");
+    }
+    @Test
+    public void testHtmlMail(){
+        Context context = new Context();
+        context.setVariable("username","Sunday");
+
+        String content = templateEngine.process("/mail/demo", context);
+        System.out.println(content);
+
+        mailClient.sendMail("ZCQvvv777@buaa.edu.cn","HTML", content);
+    }
+}
