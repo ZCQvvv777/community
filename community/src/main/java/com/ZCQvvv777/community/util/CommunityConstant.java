0200d95e9638dd1bc743992f029dde76bf47683e
ZCQvvv777 <swjtu2016@126.com>
2022-08-19 15:45:45 +0800

    实现访问注册页面的请求，即点击顶部的注册链接时，打开注册页面。 实现提交注册的请求，在服务端对提交的数据进行验证，通过后发送激活邮件。 实现激活账号的请求，在服务端验证激活码的有效性，若有效则修改账号的状态。

---
+++ community/src/main/java/com/ZCQvvv777/community/util/CommunityConstant.java
@@ -0,0 +1,22 @@
+package com.ZCQvvv777.community.util;
+
+/**
+ * @Description 常量接口
+ * @Date 2022/8/19 15:14
+ */
+public interface CommunityConstant {
+    /**
+     * 激活成功
+     */
+    int ACTIVATION_SUCCESS = 0;
+
+    /**
+     * 重复激活
+     */
+    int ACTIVATION_REPEAT = 1;
+
+    /**
+     * 激活失败
+     */
+    int ACTIVATION_FAILURE = 2;
+}
